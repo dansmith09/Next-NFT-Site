bc19b315c9ccac5c1817fc9b1d69c27e909dbe47
dansmith09 <dansmith_09@hotmail.com>
2023-05-14 13:24:23 +1000

    First commit

---
+++ src/app/components/background.js
@@ -0,0 +1,39 @@
+import { FaEthereum } from 'react-icons/fa'
+
+export default function Background() {
+  return (
+    <div className='background'>
+        <div className='container'>
+            <div className='bubbles'>
+                
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+                <span style={{ "--i": Math.floor(Math.random() * 120 + 100) }}><FaEthereum className='eth'/></span>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Background from './components/background'
+import { FaEthereum } from 'react-icons/fa'
+
+export default function Home() {
+  return (
+    <main>
+      <Background />
+    </main>
+  )
+}
