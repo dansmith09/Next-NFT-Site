3f4037e0ff5dfb97a9fcbc92f48b35805c4a1c24
dansmith09 <dansmith_09@hotmail.com>
2023-05-15 02:52:23 +1000

    about treasury animation finished

--- src/app/components/AboutImage.js
+++ src/app/components/AboutImage.js
@@ -1,24 +1,56 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BsArrowLeftCircle } from 'react-icons/bs'
+import { BsArrowRightCircle } from 'react-icons/bs'
 
 export default function AboutImage() {
 
     const [aboutNumber, setAboutNumber] = useState(1)
 
     const aboutNext = () => {
+        if(aboutNumber == 8) {
+            return
+        }
         const newNum = aboutNumber + 1
         setAboutNumber(newNum)
     }
     const aboutPrevious = () => {
+        if(aboutNumber == 1) {
+            return
+        }
         const newNum = aboutNumber - 1
         setAboutNumber(newNum)
     }
 
+    useEffect(() => {
+        document.addEventListener('keydown', detectKeyDown, true)
+    })
+
+    const detectKeyDown = (e) => {
+        if(e.key === " ") {
+            aboutNext()
+        }
+        if(e.key === "ArrowRight") {
+            aboutNext()
+        }
+        if(e.key === "ArrowLeft") {
+            aboutPrevious()
+        }
+        if(e.key === "Backspace") {
+            aboutPrevious()
+        }
+    }
+
     return (
       <div className='AboutImageContainer'>
-        <button disabled={aboutNumber < 2 ? 'disabled' : ''} onClick={aboutPrevious}> Prev </button>
-        <div className={`AboutImage about${aboutNumber}`}></div>
-        <button disabled={aboutNumber > 7 ? 'disabled' : ''} onClick={aboutNext}> Next </button>
+        <div className={`AboutImage about${aboutNumber}`}>
+            <button
+                className={`aboutBtn aboutBtnLeft ${aboutNumber == 1 ? 'hide' : ''}`}
+                onClick={aboutPrevious}> <BsArrowLeftCircle /> </button>
+            <button 
+                className={`aboutBtn aboutBtnRight ${aboutNumber == 8 ? 'hide' : ''}`}
+                onClick={aboutNext}> <BsArrowRightCircle /> </button>    
+        </div>
       </div>
     )
   }
